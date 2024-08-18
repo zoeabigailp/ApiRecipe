233bff01209c880119e8dfd378f398ef4ffce8fa
zoeabigailp <zoep5473@gmail.com>
2024-08-18 11:36:32 -0300

    Lista de Recetas

---
+++ src/main/java/com/recipe/RecipeList/RecipeListApplication.java
@@ -0,0 +1,13 @@
+package com.recipe.RecipeList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recipe/RecipeList/RecipeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.RecipeList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
